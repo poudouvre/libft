e9bb3f71706a13a2895a88b8b9500b6d6ae9b0f0
c450coleoptere <nicolasrubin@protonmail.com>
2021-05-21 14:13:57 +0200

    optimized by removing redundant counters

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: nrubin <nrubin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 19:59:46 by nrubin            #+#    #+#             */
-/*   Updated: 2021/05/15 15:37:37 by nrubin           ###   ########.fr       */
+/*   Updated: 2021/05/21 14:13:05 by nrubin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,16 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		i;
-	int		j;
 	int		len;
 	char	*ret;
 
-	i = 0;
-	j = 0;
 	if (!s1)
 		return (NULL);
-	while (s1[i] && ft_strchr(set, s1[i]))
+	while (*s1 && ft_strchr(set, *s1))
 		s1++;
 	len = ft_strlen(s1);
 	while (len && ft_strchr(set, s1[len]))
 		len--;
-	ret = ft_substr(s1, i, len + 1);
+	ret = ft_substr(s1, 0, len + 1);
 	return ((char *)ret);
 }
